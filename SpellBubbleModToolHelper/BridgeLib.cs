fa673b6428bcc5a45d96fa23827cb744742faa7a
datasone <datasone@datasone.moe>
2023-07-19 20:52:00 +0800

    Merge special rules patching with unlocking music and characters

--- SpellBubbleModToolHelper/BridgeLib.cs
+++ SpellBubbleModToolHelper/BridgeLib.cs
@@ -109,53 +109,12 @@ namespace SpellBubbleModToolHelper
             return array;
         }
 
-        [UnmanagedCallersOnly(EntryPoint = "patch_special_rules")]
-        public static void PatchSpecialRule(IntPtr shareDataPath, IntPtr outPath) // string shareDataPath, outPath
-        {
-            var path = Marshal.PtrToStringUTF8(shareDataPath);
-            var outputPath = Marshal.PtrToStringUTF8(outPath);
-            
-            var (am, bundle, assets) = LoadAssetsFromBundlePath(path);
-            var replacerList = new List<AssetsReplacer>();
-            
-            var specialRuleInfo = assets.table.GetAssetInfo("TPZ_SpecialRuleData");
-            var specialRuleBaseField = am.GetTypeInstance(assets.file, specialRuleInfo).GetBaseField();
-
-            UnlockSpecialRules(ref specialRuleBaseField);
-
-            var specialRuleNewBytes = specialRuleBaseField.WriteToByteArray();
-            var specialRuleReplacer = new AssetsReplacerFromMemory(0, specialRuleInfo.index, (int) specialRuleInfo.curFileType,
-                AssetHelper.GetScriptIndex(assets.file, specialRuleInfo), specialRuleNewBytes);
-            
-            replacerList.Add(specialRuleReplacer);
-            PatchAssetBundle(bundle, assets, replacerList, outputPath);
-        }
-
-        private static void UnlockSpecialRules(ref AssetTypeValueField baseField)
-        {
-            var excludePrefixList = new string[] { "TwoColors", "FourColors", "NONE" };
-            
-            var specialRuleList = baseField.Get("sheets").Get(0).Get(0).Get("list").Get(0).GetChildrenList();
-
-            var index = 1;
-            foreach (var specialRuleItem in specialRuleList)
-            {
-                if (excludePrefixList.Any(prefix =>
-                        specialRuleItem.Get("ID").GetValue().AsString().StartsWith(prefix))) continue;
-                
-                specialRuleItem.Get("Index").GetValue().Set(index);
-                ++index;
-                
-                specialRuleItem.Get("IsInRuleSetting").GetValue().Set(1);
-                specialRuleItem.Get("IsDefault").GetValue().Set(1);
-            }
-        }
-        
-        [UnmanagedCallersOnly(EntryPoint = "patch_music_and_character")]
-        public static void PatchMusicAndCharacter(IntPtr shareDataPath, IntPtr outPath, ArrayWrapper excludedDLCs,
-            IntPtr leftMusicID, int characterEnabled, int characterTargetDLC) 
-        // string shareDataPath, outPath, int[] excludedDLCs, string leftMusicID
-        // bool characterEnabled, int characterTargetDLC
+        [UnmanagedCallersOnly(EntryPoint = "patch_features")]
+        public static void PatchFeatures(IntPtr shareDataPath, IntPtr outPath, int patchMusic,
+                ArrayWrapper excludedDLCs, IntPtr leftMusicID, int patchCharacter, int characterTargetDLC,
+                int patchSpecialRules)
+            // string shareDataPath, string outPath, bool patchMusic, int[] excludedDLCs, string leftMusicID
+            // bool patchCharacter, int characterTargetDLC, bool patchSpecialRules
         {
             var path = Marshal.PtrToStringUTF8(shareDataPath);
             var outputPath = Marshal.PtrToStringUTF8(outPath);
@@ -165,17 +124,20 @@ namespace SpellBubbleModToolHelper
             var (am, bundle, assets) = LoadAssetsFromBundlePath(path);
             var replacerList = new List<AssetsReplacer>();
 
-            var musicInfo = assets.table.GetAssetInfo("TPZ_MusicData");
-            var musicBaseField = am.GetTypeInstance(assets.file, musicInfo).GetBaseField();
+            if (patchMusic != 0)
+            {
+                var musicInfo = assets.table.GetAssetInfo("TPZ_MusicData");
+                var musicBaseField = am.GetTypeInstance(assets.file, musicInfo).GetBaseField();
 
-            UnlockDLCsForMusic(ref musicBaseField, excludedDLCIds, leftMusic);
+                UnlockDLCsForMusic(ref musicBaseField, excludedDLCIds, leftMusic);
 
-            var musicNewBytes = musicBaseField.WriteToByteArray();
-            var musicAssetReplacer = new AssetsReplacerFromMemory(0, musicInfo.index, (int) musicInfo.curFileType,
-                AssetHelper.GetScriptIndex(assets.file, musicInfo), musicNewBytes);
-            replacerList.Add(musicAssetReplacer);
+                var musicNewBytes = musicBaseField.WriteToByteArray();
+                var musicAssetReplacer = new AssetsReplacerFromMemory(0, musicInfo.index, (int) musicInfo.curFileType,
+                    AssetHelper.GetScriptIndex(assets.file, musicInfo), musicNewBytes);
+                replacerList.Add(musicAssetReplacer);
+            }
 
-            if (characterEnabled != 0)
+            if (patchCharacter != 0)
             {
                 var characterInfo = assets.table.GetAssetInfo("TPZ_CharacterData");
                 var characterBaseField = am.GetTypeInstance(assets.file, characterInfo).GetBaseField();
@@ -187,6 +149,20 @@ namespace SpellBubbleModToolHelper
                     (int) characterInfo.curFileType, AssetHelper.GetScriptIndex(assets.file, characterInfo),
                     characterNewBytes);
                 replacerList.Add(characterAssetReplacer);
+            }
+
+            if (patchSpecialRules != 0)
+            {
+                var specialRuleInfo = assets.table.GetAssetInfo("TPZ_SpecialRuleData");
+                var specialRuleBaseField = am.GetTypeInstance(assets.file, specialRuleInfo).GetBaseField();
+
+                UnlockSpecialRules(ref specialRuleBaseField);
+
+                var specialRuleNewBytes = specialRuleBaseField.WriteToByteArray();
+                var specialRuleReplacer = new AssetsReplacerFromMemory(0, specialRuleInfo.index,
+                    (int) specialRuleInfo.curFileType,
+                    AssetHelper.GetScriptIndex(assets.file, specialRuleInfo), specialRuleNewBytes);
+                replacerList.Add(specialRuleReplacer);
             }
 
             PatchAssetBundle(bundle, assets, replacerList, outputPath);
@@ -244,6 +220,26 @@ namespace SpellBubbleModToolHelper
                 {
                     characterItem.Get("DLCIndex").GetValue().Set(characterTargetDLC);
                 }
+            }
+        }
+
+        private static void UnlockSpecialRules(ref AssetTypeValueField baseField)
+        {
+            var excludePrefixList = new string[] {"TwoColors", "FourColors", "NONE"};
+
+            var specialRuleList = baseField.Get("sheets").Get(0).Get(0).Get("list").Get(0).GetChildrenList();
+
+            var index = 1;
+            foreach (var specialRuleItem in specialRuleList)
+            {
+                if (excludePrefixList.Any(prefix =>
+                        specialRuleItem.Get("ID").GetValue().AsString().StartsWith(prefix))) continue;
+
+                specialRuleItem.Get("Index").GetValue().Set(index);
+                ++index;
+
+                specialRuleItem.Get("IsInRuleSetting").GetValue().Set(1);
+                specialRuleItem.Get("IsDefault").GetValue().Set(1);
             }
         }
 
